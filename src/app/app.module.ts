b2edb0b457f3a2e3973aeacb562df22f1492a698
courtney <courtney@fire4.com>
2018-08-10 16:47:21 +0100

    Initial commit of Cloud testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { GatewaysComponent } from './gateways/gateways.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { StatusComponent } from './gateways/status/status.component';
+import { LoginsComponent } from './gateways/logins/logins.component';
+import { ErrorHandlerService } from './error-handler.service';
+import { LoginChartService } from './gateways/logins/login-chart/login-chart.service';
+import { LoginChartComponent } from './gateways/logins/login-chart/login-chart.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GatewaysComponent,
+    StatusComponent,
+    LoginsComponent,
+    LoginChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    ErrorHandlerService,
+    LoginChartService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
